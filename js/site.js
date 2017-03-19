f10082008cead4cb79d7b64ee2daa594d512360e
eladheller <eladheller@gmail.com>
2017-03-19 07:58:40 +0200

    new route

--- js/site.js
+++ js/site.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         userArea: "updateuserdetails",
         manage: {
             upload: "uploaduserscsv",
-            download: "getuserscsv",
+            download: "usersresport.csv",
             tableRow: "confirmuserdetails",
             getTableRows: "getuserdetailsconfirms"
         }
